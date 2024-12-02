86764df27a2050bfc0620d62dea30de9d64c5b3d
emmeiwhite <emmeiwhite@gmail.com>
2024-12-02 20:17:49 +0530

    redux-1.3 actions in action with action creators

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // console.log('Getting started with Redux - standalone!')
+const redux = require('redux')
+const createStore = redux.createStore
 
 const ORDER_PIZZA = 'ORDER_PIZZA'
+const ADD_TOPPING = 'ADD_TOPPING'
 
 // action
 /* ---
@@ -13,10 +16,52 @@ const orderPizza = {
 --- */
 
 // action creator
-
 function orderPizza() {
   return {
     type: ORDER_PIZZA,
     shop_name: 'Pizza Shop'
   }
 }
+
+function updateToppings(newTopping) {
+  return {
+    type: ADD_TOPPING,
+    payload: newTopping
+  }
+}
+
+// initialState
+const initialState = {
+  pizzaBase: 100,
+  toppings: ['cheese', 'mushroom', 'capsicum']
+}
+
+// reducer
+const reducer = (state = initialState, action) => {
+  if (action.type === ORDER_PIZZA) {
+    return { ...state, pizzaBase: state.pizzaBase - 1 }
+  }
+
+  if (action.type === ADD_TOPPING) {
+    return { ...state, toppings: [...state.toppings, action.payload] }
+  }
+
+  return state
+}
+
+// 1. Let's create our store
+
+const store = createStore(reducer)
+
+// 2. store.getState()
+console.log('Initial State :', store.getState())
+
+// 3. Register listener via subscribe(listener)
+store.subscribe(() => {
+  console.log('Updated State :', store.getState())
+})
+
+// 4. Let's dispatch actions
+store.dispatch(orderPizza())
+store.dispatch(updateToppings('Chillies'))
+store.dispatch(orderPizza())
