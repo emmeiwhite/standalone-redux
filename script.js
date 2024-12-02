a07a8b0ed28cd35f4bfa19ca1fb6daa3e4bc3ac7
emmeiwhite <emmeiwhite@gmail.com>
2024-12-02 17:16:49 +0530

    redux-1.2 action creator

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Getting started with Redux - standalone!')
+// console.log('Getting started with Redux - standalone!')
+
+const ORDER_PIZZA = 'ORDER_PIZZA'
+
+// action
+/* ---
+
+const orderPizza = {
+  type: ORDER_PIZZA,
+  shop_name: 'Pizza Shop'
+}
+
+--- */
+
+// action creator
+
+function orderPizza() {
+  return {
+    type: ORDER_PIZZA,
+    shop_name: 'Pizza Shop'
+  }
+}
